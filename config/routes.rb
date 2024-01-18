4d1207c5d10f2a3a91b87c28510168dbb13e7cef
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2024-01-18 13:47:56 +0200

    delete auto generated files and edit routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :posts
-  root to: 'users#index'
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+  resources :posts, only: [:index, :show]
+end
+  
+root to: 'users#index'
 end
