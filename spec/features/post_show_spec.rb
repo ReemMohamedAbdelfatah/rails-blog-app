61397a114ed7b73e416b0ce6c0d13937ee85210c
Faiz Mohammad <email2faizm@gmail.com>
2024-02-07 15:42:21 +0530

    Update post_show_spec.rb

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -10,5 +10,29 @@ RSpec.describe 'Post Index View Page', type: :feature do
     )
   end
 
-  
+  let!(:post1) do
+    Post.create(author: user1, title: 'Post 1', text: 'Post 1 content', comments_counter: 1, likes_counter: 0)
+  end
+
+  let!(:comment1) do
+    Comment.create(post: post1, user: user1, text: 'Nice comment 1!')
+  end
+
+  let!(:comment2) do
+    Comment.create(post: post1, user: user1, text: 'Nice comment 2!')
+  end
+
+  let!(:comment3) do
+    Comment.create(post: post1, user: user1, text: 'Nice comment 3!')
+  end
+
+  let!(:comment4) do
+    Comment.create(post: post1, user: user1, text: 'Nice comment 4!')
+  end
+
+  let!(:comment5) do
+    Comment.create(post: post1, user: user1, text: 'Nice comment 5!')
+  end
+
+
 end
